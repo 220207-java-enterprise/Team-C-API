0bf5b771a18081930e71541fc52923759f9b9ef0
Andrew <Andrew.C.Saenz12@gmail.com>
2022-03-10 18:55:46 -0500

    Main driver and pom setup

--- src/main/Java/com/revature/erm/Main.java
+++ src/main/Java/com/revature/erm/Main.java
@@ -1,54 +1,10 @@
 package com.revature.erm;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class Main {
     public static void main(String[] args) {
-
-        String URL = "jdbc:postgresql://localhost:5432/postgres?currentSchema=foundations-project";
-        String username = "postgres";
-        String password = "revature";
-
-        try {
-            Connection connection = DriverManager.getConnection(URL, username, password);
-
-            System.out.println("Connection successful");
-
-            Scanner scanner = new Scanner(System.in);
-
-            String input;
-            do {
-                System.out.println("What would you like to do?");
-                System.out.print("Login: 1\nRegister: 2\nQuit: q\nEnter your choice here -->");
-                input = scanner.next();
-                switch(input){
-                    case "1":
-                        System.out.println("login");
-                        break;
-                    case "2":
-                        System.out.println("register");
-                        break;
-                    case "q":
-                        System.out.println("quit");
-                        break;
-                    default:
-                        System.out.println("You entered " + "'" + input + "'" + " which is not valid\nPlease enter a valid option\n");
-                }
-            } while (!input.equals("q"));
-
-            System.out.println("done here");
-
-        } catch (SQLException e) {
-            System.out.println("Error in connecting to PostgreSQL");
-            e.printStackTrace();
-        }
-
-
-        /*System.out.println("this is my project");
-        User testUser = new User("iambob", "bobsemail@gmail.com",
-                "asdf1234", "bill", "samolian");*/
+        SpringApplication.run(Main.class);
     }
 }
