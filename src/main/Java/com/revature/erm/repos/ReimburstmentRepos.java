61d0071f7f0e30838789df679afc706c59e85680
krthompson89 <47638444+krthompson89@users.noreply.github.com>
2022-03-14 10:55:43 -0400

    Added Repos

--- src/main/Java/com/revature/erm/repos/ReimburstmentRepos.java
+++ src/main/Java/com/revature/erm/repos/ReimburstmentRepos.java
@@ -1,4 +1,23 @@
 package com.revature.erm.repos;
 
-public interface ReimburstmentRepos {
+import com.revature.erm.models.Reimbursement;
+import com.revature.erm.models.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReimburstmentRepos extends CrudRepository<Reimbursement, String> {
+    List<Reimbursement> getReimbursementsByreimbId(double size);
+
+    @Query("from Reimbursement b where b.owner = ?1")
+    List<Reimbursement> getBootsByOwner(User owner);
+
+    @Query(
+            value = "SELECT * from reimbursement where material = ?1",
+            nativeQuery = true
+    )
+    List<Reimbursement> findReimbursementByMaterial(String material);
 }
