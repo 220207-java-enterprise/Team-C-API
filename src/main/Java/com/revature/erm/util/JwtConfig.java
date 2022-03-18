34de92765b1bd0a286afc1dc23d7d223a3c44f87
Andrew <Andrew.C.Saenz12@gmail.com>
2022-03-18 17:11:06 -0400

    Changed properties salt from properties file, ideally we would .git ignore properties file for security

--- src/main/Java/com/revature/erm/util/JwtConfig.java
+++ src/main/Java/com/revature/erm/util/JwtConfig.java
@@ -5,25 +5,31 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import java.io.FileReader;
+import java.io.IOException;
 import java.security.Key;
+import java.util.Properties;
 
 @Component
 public class JwtConfig {
 
-    private final String salt = "dont-hard-code-salt-values"; // use a Properties file instead
+    private Properties property = new Properties();
     private int expiration = 60 * 60 * 1000; // number of milliseconds in an hour
     private final SignatureAlgorithm sigAlg = SignatureAlgorithm.HS256;
     private final Key signingKey;
 
     public JwtConfig() {
-        String salt = "don't-hard-code-salt-values"; //Added by Khari
-        byte[] saltyBytes = DatatypeConverter.parseBase64Binary(salt);
+        try {
+            property.load(new FileReader("src/main/resources/application.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] saltyBytes = DatatypeConverter.parseBase64Binary(property.getProperty("salt"));
         signingKey = new SecretKeySpec(saltyBytes, sigAlg.getJcaName());
+
     }
 
     public int getExpiration() {
-        // number of milliseconds in an hour //Added by Khari
-        int expiration = 60 * 60 * 1000;  //Added by Khari
         return expiration;
     }
 
